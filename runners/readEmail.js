176663f3094743f806f47c0912a44e308004758e
Bin Li <bin@cloudconformity.com>
2020-08-21 23:55:22 +1000

    send ms-teams message

--- runners/readEmail.js
+++ runners/readEmail.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
-const simpleParser = require('mailparser').simpleParser;
-(async ()=>{
+const simpleParser = require("mailparser").simpleParser;
+(async () => {
 	const s3 = new AWS.S3();
 	const request = {
 		Bucket: "incoming.disposable.v2.happyeme.com",
@@ -11,14 +11,14 @@ const simpleParser = require('mailparser').simpleParser;
 		const data = await s3.getObject(request).promise();
 		// console.log('Raw email:' + data.Body);
 		const email = await simpleParser(data.Body);
-		console.log('date:', email.date);
-		console.log('subject:', email.subject);
-		console.log('body:', email.text);
-		console.log('to:', email.to.text);
+		console.log("date:", email.date);
+		console.log("subject:", email.subject);
+		console.log("body:", email.text);
+		console.log("to:", email.to.text);
 
-		console.log('from:', email.from.text);
-		console.log('attachments:', email.attachments);
-		return { status: 'success' };
+		console.log("from:", email.from.text);
+		console.log("attachments:", email.attachments);
+		return { status: "success" };
 	} catch (Error) {
 		console.log(Error, Error.stack);
 		return Error;
